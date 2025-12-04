e22483a7b15c7e4564383cb0d5ee9ec0a967ebe8
Aaron Parandian <aparandian@apple.com>
2025-12-04 12:22:39 -0800

    02. manipulating data in py

---
+++ 02/data-sorted.py
@@ -0,0 +1,5 @@
+test_scores = [88, 92, 70, 100, 99, 70]
+test_names = ["John", "Jane", "Bob", "Alice", "Bob", "John", "Aaron", "Matt", "Nancy"]
+
+print(sorted(test_scores))
+print(sorted(test_names, reverse=True))
