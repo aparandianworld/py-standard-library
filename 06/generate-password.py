258c08a3125e9a2c4e95c9569a56f00ec7d7e827
Aaron Parandian <aparandian@apple.com>
2025-12-06 15:16:15 -0800

    06_03. gen secure strong password

--- 06/generate-password.py
+++ 06/generate-password.py
@@ -1,28 +1,32 @@
-from calendar import c
+import random
 import secrets
 import string
 
 
-def generate_temp_password(num_chars=8):
+def generate_temp_password():
+    num_chars = 8
     potential_chars = string.ascii_letters + string.digits + "@#$%^()/"
     password = "".join(secrets.choice(potential_chars) for _ in range(num_chars))
     return password
 
 
+def generate_strong_password():
+    num_chars = 16
+    potential_chars = string.ascii_letters + string.digits + "@#$%^()/"
+    password = "".join(secrets.choice(potential_chars) for _ in range(num_chars - 2))
+    password += secrets.choice(string.ascii_uppercase)
+    password += secrets.choice(string.digits)
+    password_list = list(password)
+    random.shuffle(password_list)
+    return "".join(password_list)
+
+
 def main():
-    print(f"Enter the number of characters for the password: ", end="")
-    num_chars = int(input())
-    if num_chars < 0:
-        print("Number of characters cannot be negative.")
-        return
-    if num_chars < 8:
-        print("Number of characters must be at least 8.")
-        return
-    password = generate_temp_password(num_chars)
-    if password:
-        print(f"Your temporary password is: {password}")
+    password = generate_temp_password()
+    strong_password = generate_strong_password()
+    print(f"Your temporary password is: {password}")
+    print(f"Your strong password is: {strong_password}")
 
 
 if __name__ == "__main__":
     main()
-c
